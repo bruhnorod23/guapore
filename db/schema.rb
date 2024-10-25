9017fe98d5c309f50ea7fedd6e36d1cbeada16c5
Bruno Rodrigues da Silva <bruno.silva@GUAPOREVIDROS.RO>
2024-10-25 11:39:07 -0400

    teste 2.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_125645) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_25_153658) do
   create_table "dados", force: :cascade do |t|
     t.string "unidade_fornecedor"
     t.string "vendedor_fornecedor"
